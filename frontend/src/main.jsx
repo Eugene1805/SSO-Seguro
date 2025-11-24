88504d8754613f40ad7ab32ace2b980b395c0382
UrielCendon <urielcd2013@gmail.com>
2025-11-24 16:29:17 -0600

    Modificación de metadata para conectar a la autenticación

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,6 +10,15 @@ const oidcConfig = {
   redirect_uri: window.location.origin,
   response_type: "code", 
   scope: "openid profile email",
+  
+  metadata: {
+    issuer: "https://unharrowed-nadene-prefamiliarly.ngrok-free.dev/auth/realms/demo",
+    authorization_endpoint: "https://unharrowed-nadene-prefamiliarly.ngrok-free.dev/auth/realms/demo/protocol/openid-connect/auth",
+    token_endpoint: "https://unharrowed-nadene-prefamiliarly.ngrok-free.dev/auth/realms/demo/protocol/openid-connect/token",
+    userinfo_endpoint: "https://unharrowed-nadene-prefamiliarly.ngrok-free.dev/auth/realms/demo/protocol/openid-connect/userinfo",
+    end_session_endpoint: "https://unharrowed-nadene-prefamiliarly.ngrok-free.dev/auth/realms/demo/protocol/openid-connect/logout",
+    jwks_uri: "https://unharrowed-nadene-prefamiliarly.ngrok-free.dev/auth/realms/demo/protocol/openid-connect/certs",
+  }
 };
 
 ReactDOM.createRoot(document.getElementById('root')).render(
